dc99aae9bcf1687adfc0833e9065199ac423826a
paresiqbal <thecrowdel@gmail.com>
2024-05-27 19:45:46 +0700

    Update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.BASE_URL;
   const curDate = new Date().toISOString();
 
-  const routes = ["", , "/projects", "/about"].map((route) => ({
+  const routes = ["", "/projects", "/about"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: curDate,
   }));
